4f04956262ffb77ee600e4dbced529f1e1ec584b
Aharu <aharuuuuu@gmail.com>
2025-05-11 20:58:22 +0800

    Added localhost multiplayer chat

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
-var peer = ENetMultiplayerPeer.new()
-var usrnm: String
-@onready var username: LineEdit = $start_menu/CenterContainer/start_container/username
 @export var seeker_scene: PackedScene
 @export var hider_scene: PackedScene
+
+var peer = ENetMultiplayerPeer.new()
+
+@onready var username: LineEdit = $start_menu/CenterContainer/start_container/username
+@onready var message: LineEdit = $chat_ui/message
+@onready var messages: TextEdit = $chat_ui/messages
+var usrnm: String
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("back_key") and not $start_menu.visible:
@@ -29,19 +34,31 @@ func _on_seeker_button_pressed() -> void:
 	peer.create_server(1027)
 	get_tree().set_multiplayer(SceneMultiplayer.new(), self.get_path())
 	multiplayer.multiplayer_peer = peer
+	joined()
 	spawn_seeker(1)
-	usrnm = username.text
-	$start_menu.visible = false
 	
 func _on_hider_button_pressed() -> void:
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(1027)
+	peer.create_client("127.0.0.1", 1027)
 	get_tree().set_multiplayer(SceneMultiplayer.new(), self.get_path())
 	multiplayer.multiplayer_peer = peer
+	joined()
 	spawn_hider(1)
-	usrnm = username.text
-	$start_menu.visible = false
 	
 func _on_back_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
 	
+func joined():
+	usrnm = username.text
+	$start_menu.visible = false
+	$chat_ui.visible = true
+
+# CHAT
+@rpc ("any_peer", "call_local")
+func msg_rpc(usrnm,data):
+	messages.text += str(usrnm, ": ", data, "\n")
+	messages.scroll_vertical = messages.get_line_count()
+
+func _on_send_pressed() -> void:
+	rpc("msg_rpc", usrnm, message.text)
+	message.clear()
